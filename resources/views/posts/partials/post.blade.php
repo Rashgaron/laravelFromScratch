4180bb8efe13f37a9ed0073f16d6ba5c263db440
Rashgaron <45242200+daniel.rodriguez.zamorano@estudiantat.upc.edu>
2022-11-11 09:26:34 +0100

    Section 21 finished

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -3,6 +3,12 @@
     <div>{{ $post->content }}</div>
 </a>
 
+@if($post->comments_count)
+    <p>{{ $post->comments_count }} comments</p>
+@else
+    <p>No comments yet!</p>
+@endif
+
 <div class="mb-3">
     <a href="{{ route('posts.edit', ['post' => $post->id]) }}"  class="btn btn-primary">Edit</a>
     <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
