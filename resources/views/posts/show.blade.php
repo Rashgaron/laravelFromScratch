d822cfacf0860de3ffe289755c08f2d8649f80dc
Rashgaron <45242200+daniel.rodriguez.zamorano@estudiantat.upc.edu>
2022-11-11 10:58:20 +0100

    Section 22 details

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,6 +12,7 @@
     <h2>Comments: </h2>
     @forelse ($post->comments as $comment)
         <p>{{ $comment->content }}</p>
+        <p>Added {{ $comment->created_at->diffForHumans() }}</p>
     @empty
         <p>No comments yet</p>
     @endforelse
