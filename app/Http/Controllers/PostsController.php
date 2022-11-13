7926f6cded99bfd9ddfe1c0340031f4921705fa6
Rashgaron <45242200+daniel.rodriguez.zamorano@estudiantat.upc.edu>
2022-11-13 19:18:34 +0100

    Adding basic cache

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\BlogPost;
 use App\Models\User;
 use App\Http\Requests\StorePost;
-use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Cache;
 class PostsController extends Controller
 {
 
@@ -23,13 +21,24 @@ class PostsController extends Controller
      */
     public function index()
     {
+        $mostCommented = Cache::remember('mostCommented', now()->addSeconds(10), function(){
+            return BlogPost::mostCommented()->take(5)->get();
+        });
+
+        $mostActive = Cache::remember('mostActive', now()->addSeconds(10), function(){
+            return User::withMostBlogPosts()->take(5)->get();
+        });
+
+        $mostActiveLastMonth = Cache::remember('mostActiveLastMonth', now()->addSeconds(10), function(){
+            return User::withMostBlogPostsLastMonth()->take(5)->get();
+        });
         return view(
             'posts.index', 
             [
-                'posts' => BlogPost::latest()->withCount('comments')->get(),
-                'mostCommented' => BlogPost::mostCommented()->take(5)->get(), 
-                'mostActive' => User::withMostBlogPosts()->take(5)->get(),
-                'mostActiveLastMonth' => User::withMostBlogPostsLastMonth()->take(5)->get(),
+                'posts' => BlogPost::latest()->withCount('comments')->with('user')->get(),
+                'mostCommented' => $mostCommented, 
+                'mostActive' => $mostActive,
+                'mostActiveLastMonth' => $mostActiveLastMonth,
             ]
         );
     }
